9311166b2037d8a3cdadf7a8254f0e2493f133c8
Avichal Pandey <avichal2018lko@gmail.com>
2025-08-16 13:42:47 +0530

    home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter,Route,Routes } from "react-router-dom"
 import React,{ lazy} from 'react';
 const Compress =React.lazy(()=>import('../pages/compress'));
 const Bgremove =React.lazy(()=>import('../pages/bgremove'));
+const Home =React.lazy(()=>import('../pages/home'));
 
 function App() {
   return (
     <BrowserRouter>
     <div className="min-h-screen w-full bg-blue-100 lg:pl-32 pt-10 flex justify-center items-start">
     <Routes>
+      <Route path='/' element={<Home />}/>
       <Route path='/compress' element={<Compress />}/>
       <Route path='/bgremove' element={<Bgremove/>}/>
     </Routes>
